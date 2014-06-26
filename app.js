9766076b617908fe43fc5326841dc85f1fc0a9a4
Baranluch <pashman98@yandex.ru>
2014-06-26 21:36:26 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var WebSocketServer = new require('ws');
+
 app.configure(function() {
     app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
     app.set('view engine', 'html')                      
@@ -19,3 +21,33 @@ app.get('/portfolio', function(req, res){ // Обрабатываем запро
 var port = process.env.PORT || 5000;       
 app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
 console.log("Listening at " + port)        // Пишем в консоль, что запустились
+
+var clients = {};
+var idNum = 0;
+
+// WebSocket-сервер на порту 8081
+
+var webSocketServer = new WebSocketServer.Server({port: 8081});
+
+webSocketServer.on('connection', function(ws) {
+    idNum += 1;
+
+    var id = "USER" + idNum;
+
+    clients[id] = ws;
+
+    console.log("новое соединение " + id);
+
+    ws.on('message', function(message) {
+
+        console.log('получено сообщение ' + message);
+        for(var key in clients) {
+            clients[key].send(id + ";" + message);
+        }
+    });
+
+    ws.on('close', function() {
+        console.log('соединение закрыто ' + id);
+        delete clients[id];
+    });
+});
